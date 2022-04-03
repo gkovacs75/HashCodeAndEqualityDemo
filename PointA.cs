81bd004bab37e985862be897ebd878f5777ac0f9
Gabor <Gabor@MSI>
2022-04-03 09:27:44 -0400

    Add project files.

---
+++ PointA.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeDemo
+{
+    public class PointA : IEquatable<PointA>
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public PointA(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public bool Equals(PointA? other)
+        {
+            Console.Write(" Calling Equals(PointA? other): ");
+
+            return other != null && this.GetType() == other.GetType() && other.X == this.X && other.Y == this.Y;
+        }
+
+        public override bool Equals(object obj)
+        {            
+            Console.Write(" Calling Equals(object obj): ");
+
+            return Equals(obj as PointA);
+        }
+
+        public override int GetHashCode()
+        {            
+            // When comparing 2 objects, the system will use a hash table to see if 2 objects 
+            // have the same hashcode. This is much faster than calling .Equals() over and over.
+            // Once a match is found (2 objects have the same hashcode), then the system will 
+            // call .Equals() just to be sure that they are in fact the same object. This is because
+            // 2 objects could have the same hash code. Since ints are 32 bits in .net, that means 
+            // that we have 2^32 possible hash codes--about 4 billion. 
+
+            Console.Write(" Calling GetHashCode(): ");
+
+            // HashCode allows you to combine up to 8 elements
+            return HashCode.Combine(this.X, this.Y);
+
+            // If you have more than 8 items, do the following:
+            // HashCode hc = new HashCode();
+            // hc.Add(this.X);
+            // hc.Add(this.Y);
+            // etc...
+            // return hc.ToHashCode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using HashCodeDemo;
+
+
+PointA p1 = new PointA(100, 100);
+PointA p1b = new PointA(100, 100);
+PointA p2 = new PointA(100, 200);
+PointA p3 = new PointA(100, 300);
+PointA p4 = new PointA(100, 400);
+
+// Test object equality
+Console.Write("p1 == p1. Expect: True. Result: ");
+Console.WriteLine(p1 == p1);
+Console.WriteLine();
+
+// Test object equality
+Console.Write("p1 == p1b. Expect: False. Result: ");
+Console.WriteLine(p1 == p1b);
+Console.WriteLine();
+
+// Test values equality
+Console.Write("p1.Equals(p1). Expect: True. Result: ");
+Console.WriteLine(p1.Equals(p1));
+Console.WriteLine();
+
+// Test values equality
+Console.Write("p1.Equals(p2). Expect: False. Result: ");
+Console.WriteLine(p1.Equals(p2));
+Console.WriteLine();
+
+
+
+Console.WriteLine();
+
+// Test Hash Code
+Dictionary<PointA, string> dic = new Dictionary<PointA, string>();
+
+Console.Write("dic.Add(p1, 'p1')");
+dic.Add(p1, "p1");
+
+Console.WriteLine();
+
+Console.Write("dic.Add(p2, 'p2')");
+dic.Add(p2, "p2");
+
+Console.WriteLine();
+
+Console.Write("dic.ContainsKey(p1b). Expect: True. Result:  ");
+Console.WriteLine(dic.ContainsKey(p1b));
+Console.WriteLine();
+Console.Write("dic.ContainsKey(p4). Expect: False. Result:  ");
+Console.WriteLine(dic.ContainsKey(p4));
+
